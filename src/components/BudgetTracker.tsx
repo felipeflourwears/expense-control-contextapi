433e8a0857e847ab841298b5589bbf07fd2e01a2
felipeflourwears <72992664+felipeflourwears@users.noreply.github.com>
2024-10-24 15:44:25 -0600

    Component Amount Display && View Front Budget

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import AmountDisplay from "./AmountDisplay"
 
 const BudgetTracker = () => {
   return (
-    <div>BudgetTracker</div>
+    <div className='grid grid-cols-1 md:grid-cols2 gap-5'>
+        <div className='flex justify-center'>
+            <img src='/grafico.jpg' alt="Grafico de gastos"/>
+        </div>
+        <div className="flex flex-col justify-center items-center gap-8">
+            <button
+                type='button'
+                className="bg-pink-600 w-full p-2 text-white uppercase font-bold rounded-lg"
+            >
+                Reset App
+            </button>
+            <AmountDisplay 
+                label="Budget"
+                amount={300}
+            />
+            <AmountDisplay 
+                label="Avalaible"
+                amount={300}
+            />
+            <AmountDisplay 
+                label="Lost"
+                amount={300}
+            />
+        </div>
+    </div>
   )
 }
 
